9de749978289998f46e0a36bbc01ae9ad05fe417
nyinyizin <nyinyizin@triosys.info>
2025-08-05 22:40:06 +0630

    email-confirm

---
+++ app/confirm-email/page.tsx
@@ -0,0 +1,8 @@
+import ConfirmEmail from "@/components/auth/confirm-email";
+import React from "react";
+
+const ConfirmEmailPage = () => {
+  return <ConfirmEmail />;
+};
+
+export default ConfirmEmailPage;
