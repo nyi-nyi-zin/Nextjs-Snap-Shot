2f1b65175e85ca703927aa689574cf29a9b2c052
nyinyizin <nyinyizin@triosys.info>
2025-08-25 09:49:48 +0630

    home-page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import { Button } from "@/components/ui/button";
+import Products from "@/components/products";
+import { db } from "@/server";
+import { productVariants } from "@/server/schema";
 
-export default function Home() {
+export default async function Home() {
+  const productwithVariants = await db.query.productVariants.findMany({
+    with: {
+      variantImages: true,
+      variantTags: true,
+      product: true,
+    },
+    // orderBy: (productVariants, { desc }) => [desc(productVariants.id)],
+  });
+
   return (
     <main>
-      <Button>Hello World</Button>
+      <h2>Nav</h2>
+      <Products productWithVariants={productwithVariants} />
     </main>
   );
 }
---
+++ components/products/index.tsx
@@ -0,0 +1,40 @@
+import formatCurrency from "@/lib/formatCurrency";
+import { VariantsWithProduct } from "@/lib/inter-types";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type ProductsProps = {
+  productWithVariants: VariantsWithProduct[];
+};
+const Products = ({ productWithVariants }: ProductsProps) => {
+  return (
+    <main className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+      {productWithVariants.map((p) => {
+        return (
+          <Link
+            key={p.id}
+            className="bg-white p-2 rounded-md "
+            href={`/products/${p.id}?productId=${p.product.id}type=${p.productType}&image=${p.variantImages[0].image_url}&title=${p.product.title}&price=${p.product.price}`}
+          >
+            <Image
+              src={p.variantImages[0].image_url}
+              alt={p.product.title}
+              width={600}
+              height={400}
+            />
+            <hr className="my-2" />
+            <h3 className="font-semibold">
+              {p.product.title.substring(0, 26) + "..."}
+            </h3>
+            <p className="font-medium text-sm mt-1">
+              {formatCurrency(p.product.price)} MMK
+            </p>
+          </Link>
+        );
+      })}
+    </main>
+  );
+};
+
+export default Products;
